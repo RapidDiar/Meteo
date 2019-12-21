2f223bc826e78f3ad6bcf465e8cca2c8b154a7a9
RapidDiar <shol02rgb@gmail.com>
2019-12-21 21:47:24 +0600

    Update 21.12.2019 24:47

--- All_in_one.py
+++ All_in_one.py
@@ -1,11 +1,24 @@
 import pandas as pd
+import re
 
-excel_header = ['Дата','средняя','осадков, мм',]
+excel_header = ['Дата','средняя','осадков, мм']
+list_month = ['января','февраля','марта','апреля','мая','июня','июля','августа','сентября','октября','ноября','декабря']
 word_dict = pd.DataFrame()
-excel_read = pd.read_excel('Диана Петропавл темп123.xlsx',sheet_name='Лист1')
+excel_read = pd.read_excel('1900-1995.xlsx',sheet_name='Лист1')
 
 for header in excel_header:
     word_dict[header] = [excel_read[header][words] for words in excel_read.index]
+
+for date in word_dict.index:
+    for month in range(len(list_month)):
+        insert_date = re.sub(r'\D'+list_month[month]+'\D','.'+str(month+1)+'.',word_dict['Дата'][date])
+        if re.findall(r'\D[а-я]',insert_date) == []:
+            break
+    word_dict['Дата'][date] = insert_date
+
+
+
+
 
 
 
--- Main.py
+++ Main.py
@@ -1,10 +1,17 @@
 import pandas as pd
 import matplotlib.pyplot as plt
 
-df = pd.DataFrame({'x': [10.4, 8, 10, 7, 7, 10, 9, 9],
+'''df = pd.DataFrame({'x': [10.4, 8, 10, 7, 7, 10, 9, 9],
                    'y': [6, 4, 5, 5, 7, 10, 9, 9]})
 mass = [i for i in range(64) ]
 print(mass)
 
 df.plot()
-plt.show()
+plt.show()'''
+
+import re
+string = 'декабря'
+date = '31 декабря 1995'
+#result = re.sub(r'\D'+string+'\D','.12.',date)
+result = re.findall(r'\D[а-я]',date)
+print(result)
